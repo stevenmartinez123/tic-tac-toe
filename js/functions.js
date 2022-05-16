4dfc191b58cb58f208d8a8f759a6d91ee6cb3f9c
Steven Martinez <stevenmartinez1494@gmail.com>
2022-05-15 22:01:30 -0700

    function added to check if winner. if winner board disappears

--- js/functions.js
+++ js/functions.js
@@ -34,17 +34,36 @@ function XorO() {
         game[index - 1] = 'X';
         this.style.backgroundColor = '#F99273'; 
         playerOne.turn = false;
+        winCheck(game[index - 1]);
     } else {
         this.textContent = 'O';
         game[index - 1] = 'O';
         this.style.backgroundColor = '#61AAB3'; 
         playerOne.turn = true;
+        winCheck(game[index - 1]);
     }
 
-    winCheck();
 };
 
-function winCheck() {
+function winCheck(letter) {
+
+    if  ((game[0] == letter && game[1] == letter && game[2] == letter) || //horizontal win 1st row
+    (game[3] == letter && game[4] ==  letter && game[5] == letter) ||  //horizontal win 2nd row
+    (game[6] == letter && game[7] == letter && game[8] == letter) || //horizontal win 3rd row
+    (game[0] == letter && game[3] == letter && game[6] == letter) || //vertical win first row left side
+    (game[1] == letter && game[4] == letter && game[7] == letter) || //vertical win center
+    (game[2] == letter && game[5] == letter && game[8] == letter) || //vertical win third row right side
+    (game[0] == letter && game[4] == letter && game[8] == letter) || //diagnol win left
+    (game[2] == letter && game[4] == letter && game[6] == letter)) { //diagnol win right
+
+        let board = document.querySelector('.gameboard');
+        board.style.transition = '1s';
+        board.style.opacity = 0;
+        gameOver(letter);
+    } 
+};
+
+function gameOver(letter) {
     
 };
 
