215872b6501a2ef8f39f8049a066384403fc3f47
Steven Martinez <stevenmartinez1494@gmail.com>
2022-05-15 21:09:32 -0700

    board now switches between x and o when clicking a div

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,51 @@
 function Player(name) {
     return {
-        name
+        name, 
+        turn: true,
+        winner: true
     };
 };
 
-(function gameBoard() {
-    var game = new Array(9);
+const playerOne = Player('Bob');
+const playerTwo = Player('Jim');
+const game = new Array(9);
+
+(function populateGameBoard() {
     var board = document.querySelector('.gameboard');
     for (let i = 0; i < game.length; i++) {
         let div = document.createElement('div');
         div.className = 'gameboard-div';
         div.id = `boardID-${i+1}`;
         board.appendChild(div);
-        div.textContent = `${i + 1}`;
+        div.addEventListener('click', XorO)
     }
 })();
+
+
+function XorO() {
+    if (this.textContent.toString().length > 0) {
+        return;
+    }
+
+    let index = this.id.toString().substring(8);
+
+    if (playerOne.turn == true) {
+        this.textContent = 'X';
+        game[index - 1] = 'X';
+        this.style.backgroundColor = '#F99273'; 
+        playerOne.turn = false;
+    } else {
+        this.textContent = 'O';
+        game[index - 1] = 'O';
+        this.style.backgroundColor = '#61AAB3'; 
+        playerOne.turn = true;
+    }
+
+    winCheck();
+};
+
+function winCheck() {
+    
+};
+
 
